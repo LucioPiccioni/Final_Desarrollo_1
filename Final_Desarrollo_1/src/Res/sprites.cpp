951d1e5c5a90209ee8f3e45682735e21f40a5748
LucioPiccioni <luciostefanopiccioni@gmail.com>
2024-12-03 18:43:28 -0300

    Added Sprites init & uninit

--- Final_Desarrollo_1/src/Res/sprites.cpp
+++ Final_Desarrollo_1/src/Res/sprites.cpp
@@ -2,5 +2,31 @@
 
 namespace SPRITE
 {
-	
+	const int animationTotalFrames = 11;
+
+	Sprites sprites = {};
+	SpriteMovement spritesMovement = {};
+
+	void init()
+	{
+		sprites.player_Idle = LoadTexture("res/Idle (32x32).png");
+		sprites.player_Run = LoadTexture("res/Run (32x32).png");
+		sprites.player_Jump = LoadTexture("res/Jump (32x32).png");
+
+		sprites.platform = LoadTexture("res/platform.png");
+
+		sprites.space = LoadTexture("res/Paralax/space.png");
+	}
+
+	void unload()
+	{
+		UnloadTexture(sprites.player_Idle);
+		UnloadTexture(sprites.player_Run);
+		UnloadTexture(sprites.player_Jump);
+
+		UnloadTexture(sprites.platform);
+
+		UnloadTexture(sprites.space);
+	}
+
 }
