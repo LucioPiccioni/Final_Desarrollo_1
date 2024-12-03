3e389d199737591a1a37cd8425816fcc76ac6a98
LucioPiccioni <luciostefanopiccioni@gmail.com>
2024-12-03 19:09:02 -0300

    Added input manager in Player

--- Final_Desarrollo_1/src/Enities/player.cpp
+++ Final_Desarrollo_1/src/Enities/player.cpp
@@ -1,9 +1,37 @@
 #include "player.h"
 
+#include "raylib.h"
+
+#include "Program/program_Data.h"
+#include "Res/sprites.h"
+
 namespace PLAYER
 {
 	Player player = {};
 
 	float actual_Gravity = min_Gravity;
 
+	void movement_Input(float delta_Time)
+	{
+		player.input_Pressed = false;
+
+		if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT))
+		{
+			player.speed.x -= movement_Speed * delta_Time;
+			player.input_Pressed = true;
+		}
+
+		if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT))
+		{
+			player.speed.x += movement_Speed * delta_Time;
+			player.input_Pressed = true;
+		}
+
+		if ((IsKeyPressed(KEY_W) || IsKeyPressed(KEY_UP) || IsKeyPressed(KEY_SPACE)) && player.on_Floor)
+		{
+			player.speed.y = jump_Force;
+			player.on_Floor = false;
+			player.platform_Standing = nullptr;
+		}
+	}
 }
