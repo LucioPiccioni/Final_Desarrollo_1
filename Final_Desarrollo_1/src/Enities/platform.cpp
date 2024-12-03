3dbb193bf85f55ec6866cef21e801ab819d3dfc5
LucioPiccioni <luciostefanopiccioni@gmail.com>
2024-12-03 19:05:45 -0300

    Added Platform spawner logic & Library

--- Final_Desarrollo_1/src/Enities/platform.cpp
+++ Final_Desarrollo_1/src/Enities/platform.cpp
@@ -1,8 +1,60 @@
 #include "platform.h"
 
+#include "raylib.h"
+
+#include "Program/program_Data.h"
+
 namespace PLATFORM
 {
 	float speed_y = min_Speed.y;
 	float actual_length = max_length;
+
+	Platform spawner()
+	{
+		const float height = 20;
+
+		Platform newPlatform;
+
+		newPlatform.active = true;
+
+		newPlatform.rect_Pos.width = actual_length;
+
+		newPlatform.rect_Pos.height = height;
+
+		newPlatform.rect_Pos.x = static_cast<float>(
+			GetRandomValue(0, static_cast<int>(PROGRAM_DATA::screenWidth - newPlatform.rect_Pos.width)));
+
+		newPlatform.speed.y = PLATFORM::speed_y;
+
+		if (GetRandomValue(0, 1) == 1)
+		{
+			newPlatform.moves_In_x = true;
+
+			newPlatform.speed.x = static_cast<float>(
+				GetRandomValue(static_cast<int>(PLATFORM::min_Speed.x), static_cast<int>(PLATFORM::max_Speed.x)));
+
+			if (GetRandomValue(0, 1) == 1)
+			{
+				newPlatform.right_Movement = true;
+				newPlatform.left_Movement = false;
+			}
+			else
+			{
+				newPlatform.left_Movement = true;
+				newPlatform.right_Movement = false;
+			}
+		}
+
+		newPlatform.rect_Pos.y = -newPlatform.rect_Pos.height;
+
+		newPlatform.rectDest.x = 0;
+		newPlatform.rectDest.y = 0;
+		newPlatform.rectDest.width = newPlatform.rect_Pos.width;
+		newPlatform.rectDest.width = newPlatform.rect_Pos.height;
+
+		return newPlatform;
+	}
+
+
 
 }
